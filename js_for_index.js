e7bd1afde7157421e8c7017d1b7dbc52c17f59df
Austin Reed <acreed1998@gmail.com>
2018-12-21 13:41:56 -0600

    "Started work on Making You Special"

--- js_for_index.js
+++ js_for_index.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
   console.log(true);
   $.getJSON('json_for_index.json', function (data) {
-    console.log(data);
+    console.log(data.opening);
+    let openingtext = $("<div>");
+    openingtext.html(data.opening);
+    $('body').append(openingtext);
   });
 });
