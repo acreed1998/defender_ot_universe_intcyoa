72a817cbd856981db3e62cc068e64e2f8d49f57a
Austin Reed <acreed1998@gmail.com>
2018-12-21 20:42:13 -0600

    "Finished Abilities Section"

--- js_for_index.js
+++ js_for_index.js
@@ -1,9 +1,10 @@
 $(document).ready(function () {
-  console.log(true);
   $.getJSON('json_for_index.json', function (data) {
-    console.log(data.opening);
-    let openingtext = $("<div>");
-    openingtext.html(data.abilities.choices.special_ops.text);
-    $('body').append(openingtext);
+    console.log(data.abilities.choices);
+    for (let key in data.abilities.choices) {
+      let openingtext = $("<div>");
+      openingtext.html(data.abilities.choices[key].text);
+      $('body').append(openingtext);
+    }
   });
 });
