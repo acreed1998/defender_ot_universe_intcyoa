af389801575a780ee266daa2f3e248f355b61a47
Austin Reed <acreed1998@gmail.com>
2018-12-31 11:09:01 -0600

    "Added the Races"

--- js_for_index.js
+++ js_for_index.js
@@ -14,6 +14,22 @@ $(document).ready(function () {
     for (let key in data.special.choices) {
       let specialSectionTableCell = $('<td>').html(data.special.choices[key]).addClass('noChoice').appendTo(specialSectionTableRow);
     }
+
+    // Race Section
+    let raceSection = $("<section>").appendTo('body');
+    let raceSectionOD = $("<div>").addClass('dialogue').html(data.race.opening).appendTo(raceSection);
+    let raceSectionTable = $("<table>").addClass("choiceTable").appendTo(raceSection);
+    let raceKeys = Object.keys(data.race.choices);
+    for (let i = 0; i < raceKeys.length; i++) {
+      if ((i) % 3 === 0 || i === 0) {
+        var raceSectionTableRow = $("<tr>").appendTo(raceSectionTable);
+      }
+      let raceSectionTableCell = $("<td>").html(data.race.choices[raceKeys[i]].text).addClass('noChoice').appendTo(raceSectionTableRow);
+    }
+
+    // Abilities Section
+    
+
     for (let key in data.abilities.choices) {
       let openingtext = $("<div>");
       openingtext.html(data.abilities.choices[key].text);
