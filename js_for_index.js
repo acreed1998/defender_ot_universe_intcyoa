40edcdf2e92b7ffa7d050a62f72a84b77a799cd4
Austin Reed <acreed1998@gmail.com>
2019-01-20 21:27:38 -0600

    navbar improvement?

--- js_for_index.js
+++ js_for_index.js
@@ -3,8 +3,9 @@ $(document).ready(function () {
   const $navigationDiv = $("<div>").addClass('navigator').appendTo('body');
   //get data from the json and use it to populate the navigation bar and add hidden sections to the body
   $.getJSON('json_for_index.json', function (data) {
+    const screenWidth = $('html')[0].scrollWidth / 7 / 7;
     _.each(data.misc_data.sections, function(section) {
-      const $navDivOptions = $('<div>').addClass('navButton').attr({ "id": `navbar-div-${section}`}).append($("<a>").attr({ "id": `navbar-a-${section}` }).text(section).hover(function () { $(this).css({ "color": "yellow" }) }, function () { $(this).css({ "color": "white" }) }).css({"cursor": "pointer"}).click(function(){showSection(section)})).appendTo($navigationDiv);
+      const $navDivOptions = $('<div>').addClass('navButton').css({"padding" : `${screenWidth}px`}).attr({ "id": `navbar-div-${section}`}).append($("<a>").attr({ "id": `navbar-a-${section}` }).text(section).hover(function () { $(this).css({ "color": "yellow" }) }, function () { $(this).css({ "color": "white" }) }).css({"cursor": "pointer"}).click(function(){showSection(section)})).appendTo($navigationDiv);
       const $bodySection = $('<section>').addClass('bodySection').attr({"id" : `secion-${section}`}).css({"display" : "none"}).appendTo('body');
     });
   });
