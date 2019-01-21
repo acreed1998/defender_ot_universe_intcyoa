5df80a4246388c51abcd3e4494248d742b86494e
Austin Reed <acreed1998@gmail.com>
2019-01-20 22:41:33 -0600

    can now switch between sections

--- js_for_index.js
+++ js_for_index.js
@@ -5,14 +5,35 @@ $(document).ready(function () {
   //get data from the json and use it to populate the navigation bar and add hidden sections to the body
   $.getJSON('json_for_index.json', function (data) {
     const screenWidth = $('html')[0].scrollWidth / data.misc_data.sections.length / data.misc_data.sections.length;
+    const jsonRemove = removeExta;
     _.each(data.misc_data.sections, function(section) {
-      const $navDivOptions = $('<div>').addClass('navButton').css({"padding" : `${screenWidth}px`}).attr({ "id": `navbar-div-${section}`}).append($("<a>").attr({ "id": `navbar-a-${section}` }).text(section).hover(function () { $(this).css({ "color": "yellow" }) }, function () { $(this).css({ "color": "white" }) }).css({"cursor": "pointer"}).click(function(){showSection(section)})).appendTo($navigationDiv);
-      const $bodySection = $('<section>').addClass('bodySection').attr({"id" : `section-${section}`}).css({"display" : "none"}).appendTo('body');
+      const $navDivOptions = $('<div>').addClass('navButton').css({"padding" : `${screenWidth}px`}).attr({ "id": `navbar-div-${jsonRemove(section)}`}).append($("<a>").attr({ "id": `navbar-a-${jsonRemove(section)}` }).text(section).hover(function () { $(this).css({ "color": "yellow" }) }, function () { $(this).css({ "color": "white" }) }).css({"cursor": "pointer"}).click(function(){showSection(jsonRemove(section))})).appendTo($navigationDiv);
+      const $bodySection = $('<section>').addClass('bodySection').attr({"id" : `section-${jsonRemove(section)}`}).css({"display" : "none"}).appendTo('body');
     });
     //populate the You section
-    if ($('#section-You').length !== 0) {
-      $('#section-You').css({ "display": "block"});
-      $('#section-You').append($('<div>'))
+    if ($('#section-Opening').length !== 0) {
+      $('#section-Opening').css({ "display": "block"});
+      $('#section-Opening').append($('<div>').attr({"id" : "opening-div"}).html(data.opening));
     }
   });
 });
+
+function removeExta(string) {
+  string = string.split("");
+  _.each(string, function(letter, index) {
+    if (letter === "&" || letter === " ") {
+      string[index] = "-";
+    }
+  });
+  return string.join("");
+}
+
+function showSection(section) {
+  const $arrayOfSections = Array.from($("section"));
+  _.each($arrayOfSections, function(functionSection, index) {
+    functionSection.style.display = "none";
+    if (functionSection.id === `section-${section}`) {
+      functionSection.style.display = "block";
+    }
+  });
+}
