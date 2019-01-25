39193062cbb4c6835074f298f1f89e7de85578e7
Austin Reed <acreed1998@gmail.com>
2019-01-24 18:30:00 -0600

    you section working more or less

--- js_for_index.js
+++ js_for_index.js
@@ -1,15 +1,16 @@
 $(document).ready(function () {
   //create the navigation bar
-  const $header = $("<hr>").addClass('def-header').text("Defender of the Universe").appendTo('body');
   const $navigationDiv = $("<div>").addClass('navigator').appendTo('body');
   //get data from the json and use it to populate the navigation bar and add hidden sections to the body
+  const $mainDiv = $("<div>").addClass('main').appendTo('body');
   $.getJSON('json_for_index.json', function (data) {
     console.log(data);
     const screenWidth = $('html')[0].scrollWidth / data.misc_data.sections.length / data.misc_data.sections.length;
     const jsonRemove = removeExta;
+    const getTheWidth = getWidth;
     _.each(data.misc_data.sections, function(section) {
       const $navDivOptions = $('<div>').addClass('navButton').css({"padding" : `${screenWidth}px`}).attr({ "id": `navbar-div-${jsonRemove(section)}`}).append($("<a>").attr({ "id": `navbar-a-${jsonRemove(section)}` }).text(section).hover(function () { $(this).css({ "color": "yellow" }) }, function () { $(this).css({ "color": "white" }) }).css({"cursor": "pointer"}).click(function(){showSection(jsonRemove(section))})).appendTo($navigationDiv);
-      const $bodySection = $('<section>').addClass('bodySection').attr({"id" : `section-${jsonRemove(section)}`}).css({"display" : "none"}).appendTo('body');
+      const $bodySection = $('<section>').addClass('bodySection').attr({"id" : `section-${jsonRemove(section)}`}).css({"display" : "none"}).appendTo($mainDiv);
     });
     //populate the Opening section
     if ($('#section-Opening').length !== 0) {
@@ -20,7 +21,7 @@ $(document).ready(function () {
       $('#section-You').append($('<div>').addClass('openingText').attr({"id" : "section-You-div-opening"}).html(data.special.opening));
       $('#section-You').append($('<div>').addClass('choices').attr({"id" : "section-You-div-choices"}));
       _.each(data.special.choices, function(choice, choiceKey) {
-        $('#section-You-div-choices').append($('<div>').addClass('choice').attr({"id" : `section-You-div-choices-${choiceKey}`}).html(choice));
+        $('#section-You-div-choices').append($('<div>').addClass('choice').css({"width" : `25%`}).attr({"id" : `section-You-div-choices-${choiceKey}`}).html(choice));
       })
     }
   });
@@ -45,3 +46,12 @@ function showSection(section) {
     }
   });
 }
+
+function getWidth(...args) {
+  let val = $('html')[0].scrollWidth;
+  console.log(args);
+  for (let i = 0; i < args; i++) {
+    val = val / args[i];
+  }
+  return val;
+} 
