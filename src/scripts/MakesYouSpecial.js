bd7d2d9cdee53823a62dd34e05349209f6245b95
Austin Reed <acreed1998@gmail.com>
2019-01-24 19:35:09 -0600

    makesyouspecial js file now works as intended

--- src/scripts/MakesYouSpecial.js
+++ src/scripts/MakesYouSpecial.js
@@ -2,8 +2,9 @@ function makesYouSpecial() {
   $.getJSON('/src/data/json_for_index.json', function(data) {
     if ($('#section-You').length !== 0) {
       $('#section-You').append($('<div>').addClass('openingText').attr({ "id": "section-You-div-special-opening" }).html(data.special.opening));
-      _.each(data.special.choices, function(choice){
-        
+      $('#section-You').append($('<div>').addClass('choices').attr({ "id": "section-You-div-choices" }));
+      _.each(data.special.choices, function(choice, choiceKey){
+        $('#section-You-div-choices').append($('<div>').addClass('choice').css({ "width": `25%` }).attr({ "id": `section-You-div-choices-${choiceKey}` }).html(choice));
       });
     }
   })
