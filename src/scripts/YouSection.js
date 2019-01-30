93b86b06e8a24873f11ecaeb7d3341d757c50f32
Austin Reed <acreed1998@gmail.com>
2019-01-29 19:30:59 -0600

    name change for a file

--- src/scripts/YouSection.js
+++ src/scripts/YouSection.js
@@ -1,14 +1,14 @@
 function makesYouSpecial() {
   $.getJSON('src/data/json_for_index.json', function(data) {
     if ($('#section-You').length !== 0) {
-          // Populate the makes you special section // 
+      // Populate the makes you special section // 
       $('#section-You').append($('<div>').addClass('openingText').attr({ "id": "section-You-div-special-opening" }).html(data.special.opening));
       $('#section-You').append($('<div>').addClass('choices').attr({ "id": "section-You-div-choices" }));
       _.each(data.special.choices, function(choice, choiceKey){
         $('#section-You-div-choices').append($('<div>').addClass('choice').css({ "width": `25%` }).attr({ "id": `section-You-div-choices-${choiceKey}` }).html(choice));
       });
 
-    // Populate the races section //
+      // Populate the races section //
       $('#section-You').append($('<div>').addClass('openingText').attr({'id' : 'section-You-div-races-opening'}).html(data.race.opening));
       $('#section-You').append($('<div>').addClass('choicesHolder').attr({'id': "section-You-div-races"}));
       let counter = 0;
@@ -36,6 +36,5 @@ function makesYouSpecial() {
         counter++;
       });
     }
-
   })
 }
