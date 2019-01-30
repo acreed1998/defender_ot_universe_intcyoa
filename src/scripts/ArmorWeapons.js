78e2502d5d647a9a6cd01f0f03423a2346cc0355
Austin Reed <acreed1998@gmail.com>
2019-01-30 09:55:04 -0600

    about to add the armor traits section

--- src/scripts/ArmorWeapons.js
+++ src/scripts/ArmorWeapons.js
@@ -1,5 +1,18 @@
 function armorWeaponsSectionFiller() {
   $.getJSON('src/data/json_for_index.json', function(data) {
-    console.log(data);
+    if ($('#section-Armor---Weapons').length !== 0) {
+      // Populate the armor opening //
+      $('#section-Armor---Weapons').append($('<div>').addClass('openingText').attr({'id' : 'section-ArmorWeapons-div-armor-opening'}).html(data.armor.opening));
+      $('#section-Armor---Weapons').append($('<div>').addClass('choices').attr({ 'id': 'section-ArmorWeapons-div-armor'}));
+      _.each(data.armor.choices, function(choice, choiceKey){
+        $('#section-ArmorWeapons-div-armor').append($('<div>').addClass('choice').css({ 'width': '33%' }).attr({ 'id': `section-ArmorWeapons-div-armor-${choiceKey}`}).html(choice.text));
+      });
+
+      // Populate the armor traits section //
+      $('#section-Armor---Weapons').append($('<div>').addClass('openingText').attr({ 'id': 'section-ArmorWeapons-div-armorTraits-opening'}).html(data.armor_traits.opening));
+      $('#section-Armor---Weapons').append($('<div>').addClass('choicesHolder').attr({ 'id': 'section-ArmorWeapons-div-armorTraits'}));
+    }
+
+    
   });
 }
