c9d1b6f62779340d7595aad7d159931a9fa6a13a
Austin <acreed1998@gmail.com>
2019-02-05 22:04:33 -0600

    added ships

--- src/scripts/Ship.js
+++ src/scripts/Ship.js
@@ -1,6 +1,12 @@
 function shipSectionFiller() {
   $.getJSON('src/data/json_for_index.json', function(data) {
     if($('#section-Ship').length !== 0) {
+      //Populate the Ship (Type) Section//
+      $("#section-Ship").append($('<div>').addClass('openingText').attr({'id' : 'section-Ship-div-ship-opening'}).html(data.ship.opening));
+      $("#section-Ship").append($('<div>').addClass('choices').attr({'id' : 'section-Ship-div-ship'}));
+      _.each(data.ship.choices, function(choice, choiceKey) {
+        $('#section-Ship-div-ship').append($('<div>').addClass('choice').css({'width' : '25%'}).attr({'id' : `section-Ship-div-ship-${choiceKey}`}).html(choice.text));
+      });
     }
   });
 }
