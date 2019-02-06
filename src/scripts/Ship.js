b48e7252e5804943fbdeb86fb1b4ee4b7dd0d9da
Austin <acreed1998@gmail.com>
2019-02-05 22:35:12 -0600

    added ship styles

--- src/scripts/Ship.js
+++ src/scripts/Ship.js
@@ -7,6 +7,20 @@ function shipSectionFiller() {
       _.each(data.ship.choices, function(choice, choiceKey) {
         $('#section-Ship-div-ship').append($('<div>').addClass('choice').css({'width' : '25%'}).attr({'id' : `section-Ship-div-ship-${choiceKey}`}).html(choice.text));
       });
+
+      //Populate the Ship Style Section//
+      $('#section-Ship').append($('<div>').addClass('openingText').attr({'id' : 'section-Ship-div-shipStyles-opening'}).html(data.ship_style.opening));
+      $('#section-Ship').append($('<div>').addClass('choicesHolder').attr({'id' : 'section-Ship-div-shipStyles'}));
+      let counter = 0;
+      let row = 0;
+      _.each(data.ship_style.choices, (choice, choiceKey) => {
+        if (counter % 3 === 0) {
+          row++;
+          $('#section-Ship-div-shipStyles').append($('<div>').addClass('choices').attr({'id' : `section-Ship-div-shipStyles-row-${row}`}));
+        }
+        $(`#section-Ship-div-shipStyles-row-${row}`).append($('<div>').addClass('choice').css({ 'width': `33%` }).attr({ 'id': `#section-Ship-div-shipStyles-${choiceKey}`}).html(choice.text));
+        counter++;
+      });
     }
   });
 }
