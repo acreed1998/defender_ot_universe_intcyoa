4b9d4c3e12ef19d517da15eec0ad328dc44b6294
Austin <acreed1998@gmail.com>
2019-02-05 21:49:47 -0600

    added ship.js file

---
+++ src/scripts/Ship.js
@@ -0,0 +1,6 @@
+function shipSectionFiller() {
+  $.getJSON('src/data/json_for_index.json', function(data) {
+    if($('#section-Ship').length !== 0) {
+    }
+  });
+}
--- src/scripts/js_for_index.js
+++ src/scripts/js_for_index.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
     }
     youSectionFiller();
     armorWeaponsSectionFiller();
+    shipSectionFiller();
   });
 });
 
