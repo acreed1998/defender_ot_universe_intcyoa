8d2f3e01359bdc158bcef8744bf35cdc39c5d3d6
Austin <acreed1998@gmail.com>
2019-02-05 19:50:13 -0600

    armor traits and weapons sections added

--- src/scripts/ArmorWeapons.js
+++ src/scripts/ArmorWeapons.js
@@ -11,6 +11,30 @@ function armorWeaponsSectionFiller() {
       // Populate the armor traits section //
       $('#section-Armor---Weapons').append($('<div>').addClass('openingText').attr({ 'id': 'section-ArmorWeapons-div-armorTraits-opening'}).html(data.armor_traits.opening));
       $('#section-Armor---Weapons').append($('<div>').addClass('choicesHolder').attr({ 'id': 'section-ArmorWeapons-div-armorTraits'}));
+      let counter = 0;
+      let row = 0;
+      _.each(data.armor_traits.choices, (choice, choiceKey) => {
+        if (counter % 4 === 0) {
+          row++;
+          $(`#section-ArmorWeapons-div-armorTraits`).append($('<div>').addClass('choices').attr({'id' : `section-ArmorWeapons-div-armorTraits-row-${row}`}));
+        }
+        $(`#section-ArmorWeapons-div-armorTraits-row-${row}`).append($('<div>').addClass('choice').css({ 'width': '25%' }).attr({ 'id': `section-ArmorWeapons-div-armorTraits-${choiceKey}`}).html(choice.text));
+        counter++;
+      });
+
+      // Populate the weapons section //
+      $('#section-Armor---Weapons').append($('<div>').addClass('openingText').attr({ 'id': 'section-ArmorWeapons-div-weapons-opening'}).html(data.weapons.opening));
+      $('#section-Armor---Weapons').append($('<div>').addClass('choicesHolder').attr({ 'id': 'section-ArmorWeapons-div-weapons'}));
+      counter = 0;
+      row = 0;
+      _.each(data.weapons.choices, (choice, choiceKey) => {
+        if (counter % 4 === 0) {
+          row++;
+          $('#section-ArmorWeapons-div-weapons').append($('<div>').addClass('choices').attr({'id' : `section-ArmorWeapons-div-weapons-row-${row}`}));
+        }
+        $(`#section-ArmorWeapons-div-weapons-row-${row}`).append($('<div>').addClass('choice').css({ 'width': '25%' }).attr({ 'id': `section-ArmorWeapons-div-weapons-${choiceKey}`}).html(choice.text));
+        counter++;
+      });
     }
 
     
