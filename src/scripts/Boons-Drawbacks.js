ca7df5d471d8cce59e250ec67da16002ed77e3a4
Austin <acreed1998@gmail.com>
2019-02-14 13:47:44 -0600

    b and d finished and added favicon

--- src/scripts/Boons-Drawbacks.js
+++ src/scripts/Boons-Drawbacks.js
@@ -1,6 +1,6 @@
 function boonsAndDrawbacksSectionFiller(data) {
   if($('#section-B-D').length !== 0) {
-    //Populate the B&D Section//
+    //Populate the Boons Section//
     $('#section-B-D').append($('<div>').addClass('openingText').attr({'id' : 'section-B-D-div-boons-opening'}).html(data.boons.opening));
     $("#section-B-D").append($('<div>').addClass('choicesHolder').attr({ 'id': 'section-B-D-div-boons'}));
     let counter = 0;
@@ -15,6 +15,18 @@ function boonsAndDrawbacksSectionFiller(data) {
       } else {
         $(`#section-B-D-div-boons-row-${row - 1}`).append($('<div>').addClass('choice').css({ 'width': '25%' }).attr({ 'id': `section-B-D-div-boons-${choiceKey}`}).html(choice.text));
       }
+      counter++;
+    });
+    $('#section-B-D').append($('<div>').addClass('openingText').attr({'id' : 'section-B-D-div-drawbacks-opening'}).html(data.drawbacks.opening));
+    $('#section-B-D').append($('<div>').addClass('choicesHolder').attr({'id' : 'section-B-D-div-drawbacks'}));
+    counter = 0;
+    row = 0;
+    _.each(data.drawbacks.choices, (choice, choiceKey) => {
+      if (counter % 4 === 0) {
+        row++;
+        $('#section-B-D-div-drawbacks').append($('<div>').addClass('choices').attr({ 'id': `section-B-D-div-drawbacks-row-${row}`}));
+      }
+      $(`#section-B-D-div-drawbacks-row-${row}`).append($('<div>').addClass('choice').css({'width' : '25%'}).attr({'id' : `section-B-D-div-drawbacks-${choiceKey}`}).html(choice.text));
       counter++;
     });
   }
