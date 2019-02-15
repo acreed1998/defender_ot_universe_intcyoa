c6de3f2b504709bcf359f6472b3885196011281f
Austin <acreed1998@gmail.com>
2019-02-14 18:22:54 -0600

    implement events section

--- src/scripts/Events.js
+++ src/scripts/Events.js
@@ -13,5 +13,29 @@ function eventsSectionFiller(data) {
       $(`#section-Events-div-events-row-${row}`).append($('<div>').addClass('choice').css({'width' : '50%'}).attr({'id' : `section-Events-div-events-${choiceKey}`}).html(choice.text));
       counter++;
     });
+
+    // Populate the unique rewards section //
+    $('#section-Events').append($('<div>').addClass('openingText').attr({'id' : 'section-Events-div-uniqueRewards-opening'}).html(data.unique_rewards.opening));
+    $('#section-Events').append($('<div>').addClass('choicesHolder').attr({ 'id': 'section-Events-div-uniqueRewards'}));
+    counter = 0;
+    row = 0;
+    const rowMarkers = [2, 4, 5, 6, 8];
+    _.each(data.unique_rewards.choices, (choice, choiceKey) => {
+      if (_.contains(rowMarkers, counter)) {
+        row = addRowC(row);
+      }
+      if (row === 2 || row === 3 || row === 5) {
+        $(`#section-Events-div-uniqueRewards-row-${row - 1}`).append($('<div>').addClass('choice').css({'width' : '75%'}).attr({'id' : `section-Events-div-${choiceKey}`}).html(choice.text));
+      } else {
+        $(`#section-Events-div-uniqueRewards-row-${row - 1}`).append($('<div>').addClass('choice').css({ 'width': '50%' }).attr({ 'id': `section-Events-div-${choiceKey}`}).html(choice.text));
+      }
+      counter++;
+    });
   }
+}
+
+function addRowC(rowNum) {
+  $('#section-Events-div-uniqueRewards').append($('<div>').addClass('choices').attr({ 'id': `section-Events-div-uniqueRewards-row-${rowNum}` }));
+  rowNum = rowNum + 1;
+  return rowNum;
 }
